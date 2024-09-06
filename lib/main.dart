0d45a2e29b3bae3b9ba15aac5803014ed5837d80
Yannick Simon <yannick.simon@gmail.com>
2024-09-06 09:47:59 +0200

    nettoyage

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:dio/dio.dart';
 
 import 'package:flutter/material.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:asn1lib/asn1lib.dart';
 
 void main() {
@@ -26,21 +25,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -52,15 +36,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -68,9 +43,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int? _iStatusCode = 0;
 
   void _timestamp() async {
+    setState(() {
+      _iStatusCode = 0;
+    });
+
     String s = "test eliaz coucou\n\n\n";
 
     Digest digest = sha256.convert(s.codeUnits);
@@ -96,13 +75,20 @@ class _MyHomePageState extends State<MyHomePage> {
         Options(headers: {'Content-Type': 'application/timestamp-query'});
 
     final dio = Dio();
-    // Créer une requête d'horodatage en utilisant l'empreinte SHA-256
+    // call digicert's timestamp server
     String tsaUrl = 'http://timestamp.digicert.com'; // URL du serveur TSA
 
-    Response response = await dio.post(tsaUrl,
-        data: timeStampReq.encodedBytes, options: options);
-
-    print(response);
+    try {
+      Response response = await dio.post(tsaUrl,
+          data: timeStampReq.encodedBytes, options: options);
+      print(response);
+      _iStatusCode = response.statusCode;
+    } on DioException catch (e) {
+      if (e.response != null) {
+        _iStatusCode = e.response!.statusCode;
+      }
+    }
+    setState(() {});
   }
 
   // for future purpose
@@ -129,13 +115,15 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'push button for timestamp',
             ),
+            if (_iStatusCode != 0)
+              Text("status code from tsa server = $_iStatusCode")
           ],
         ),
       ),
@@ -159,10 +147,14 @@ ASN1Sequence getSeqMessageImprintSequence(
 
   messageImprintSequence.add(seqAlgorithm);
 
+  //
   // why must I add this ? -- BEGIN --
+  //
   ASN1Object strange = ASN1Object.fromBytes(Uint8List.fromList([0x04, 0x20]));
   messageImprintSequence.add(strange);
+  //
   // why must I add this ? -- END --
+  //
   messageImprintSequence.add(hashedText);
 
   return messageImprintSequence;
@@ -192,9 +184,5 @@ ASN1Sequence getSeqAlgorithm() {
   ASN1Sequence seqAlgorithm = ASN1Sequence();
   seqAlgorithm.add(sha256OidHS);
   seqAlgorithm.add(paramsAsn1Obj);
-  var hexseq = seqAlgorithm.encodedBytes
-      .map((e) => "${e.toRadixString(16).padLeft(2, '0')} ")
-      .join();
-  print(hexseq);
   return seqAlgorithm;
 }
